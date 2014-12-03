def6d50b58ca6dff03c1d69c2766517cbfa0eb3c
theshadoo <siberians1@gmail.com>
2014-12-03 21:20:50 +0100

    initialize test project

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+(function($) {
+
+    var firstvar = 'variable 1';
+
+});
---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+
+    <nav>
+        <?php include 'layout/menu.html'; ?>
+    </nav>
+
+    <p>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores, molestias libero illum praesentium quis quibusdam totam aliquid minus mollitia consequuntur dolor, voluptates ex harum cupiditate in earum atque aperiam eum!
+    </p>
+
+    <ul>
+        <li>item1</li>
+        <li>item2</li>
+        <li>item3</li>
+        <li>item4</li>
+    </ul>
+
+</body>
+</html>
